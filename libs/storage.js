59cfe67d911cbbab25154bbf022ff60f48a1d17f
Eugene Popov <popov895@ukr.net>
2024-01-07 20:36:47 +0200

    Fix error reporting in the Storage

--- libs/storage.js
+++ libs/storage.js
@@ -23,7 +23,7 @@ var Storage = class {
                 const entries = JSON.parse(await this._loadFile(this._storageFile));
                 resolve(entries);
             } catch (error) {
-                reject(`Failed to load storage. ${error.message}`);
+                reject(new Error(`Failed to load storage. ${error.message}`));
             }
         });
     }
